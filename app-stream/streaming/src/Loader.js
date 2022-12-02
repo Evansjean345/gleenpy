a83ae8f2166a3442f1ecf3c85659857a21992e52
Evansjean345 <Evansjean345@gmail.com>
2022-12-02 10:15:51 +0000

    add loader

--- app-stream/streaming/src/Loader.js
+++ app-stream/streaming/src/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 import { HashLoader } from "react-spinners";
 
 function Loader() {
-  return <HashLoader color="red" size={600} speedMultiplier={1.2} />;
+  return <HashLoader color="white" size={600} speedMultiplier={1.2} />;
 }
 
 export default Loader;
