1a33746e4ceddb85e2b03a6d13eceb5cfb4886b0
Evansjean345 <Evansjean345@gmail.com>
2022-12-10 09:04:11 +0000

    jck

--- app-stream/streaming/src/components/HeroHome.jsx
+++ app-stream/streaming/src/components/HeroHome.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { useState, React } from "react";
 import "../index.css";
 import { Typewriter } from "react-simple-typewriter";
-import JCK from "../jck.mp4"
+
 
 
 
 export default function HeroHome() {
+
+  
   return (
     <>
       <header>
@@ -13,11 +15,11 @@ export default function HeroHome() {
           <video
             id="video-trailer"
             className="object-cover sm:h-[510px] w-full"
-            autoPlay
-            muted
-            loop
+            autoPlay={true}
+            muted={true}
+            loop={true}
           >
-            <source src={JCK} type="video/mp4" />
+            <source src="/jck.mp4" type="video/mp4" />
           </video>
           <div
             className=" absolute  sm:pt-32 pt-64 sm:top-12 top-0 bottom-0 flex flex-col z-40 bg-[rgba(0, 0, 0, 0.5)] text-white w-full p-20 bg-gradient-to-t from-black to-[rgba(0, 0, 0, 0.667)]"
