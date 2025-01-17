c44c0ae871a95b734e45ce540dff5b654d2937b0
evansjean345 <evansjean345@gmail.com>
2025-01-17 18:26:28 +0000

    title

--- app-stream/streaming/src/layout/ShareItems.jsx
+++ app-stream/streaming/src/layout/ShareItems.jsx
@@ -2,41 +2,43 @@ import { React } from "react";
 import { Link } from "react-router-dom";
 
 export default function ShareItems() {
-    const items = [
-      {
-        id: "1",
-        to: "/share/makosso",
-        title: `Général Makosso fait des révélations sur l'affaire "Paul Pogba"`,
-        imageSrc: "https://lifemag-ci.com/wp-content/uploads/2019/05/makosso-camille-Copier.jpg",
-        imageAlt: "",
-      },
-      {
-        id: "2",
-        to: "/share/apoutchou",
-        title: "Apoutchou National est en direct",
-        imageSrc: "https://i0.wp.com/l-frii.com/wp-content/uploads/2021/10/Cote-dIvoire-Apoutchou-National-annonce-une-tres-bonne-nouvelle-aux-blogueurs-et-influenceurs-africains.jpg?fit=720%2C683&ssl=1",
-        imageAlt: "",
-      },
-      {
-        id: "3",
-        to: "/share/hamond",
-        title: "Allô,je suis yougoss,sortez moi de là",
-        imageSrc:
-          "https://yt3.ggpht.com/HGes9hrn9xm1pv_2RiQsxHaGMvkH15wMVASJIXbIREhJYe_cF9UBnIwHPyAQ33UVN4qS6S2i=s900-c-k-c0x00ffffff-no-rj",
-        imageAlt: "",
-      },
-      {
-        id: "4",
-        to: "/share/lohoues",
-        title: "SORTEZZZ Emma Lohoues en colère répond à Carmen Sama en direct",
-        imageSrc:
-          "https://cdn.afrique-sur7.ci/700x400/articles/2022/05/emma-lohoues-son-pere-euge%CC%80ne-1er.jpg",
-        imageAlt: "",
-      },
-     ]
+  const items = [
+    {
+      id: "1",
+      to: "/share/makosso",
+      title: `Général Makosso fait des révélations sur l'affaire "Paul Pogba"`,
+      imageSrc:
+        "https://lifemag-ci.com/wp-content/uploads/2019/05/makosso-camille-Copier.jpg",
+      imageAlt: "",
+    },
+    {
+      id: "2",
+      to: "/share/apoutchou",
+      title: "Apoutchou National est en direct",
+      imageSrc:
+        "https://i0.wp.com/l-frii.com/wp-content/uploads/2021/10/Cote-dIvoire-Apoutchou-National-annonce-une-tres-bonne-nouvelle-aux-blogueurs-et-influenceurs-africains.jpg?fit=720%2C683&ssl=1",
+      imageAlt: "",
+    },
+    {
+      id: "3",
+      to: "/share/hamond",
+      title: "Allô,je suis yougoss,sortez moi de là",
+      imageSrc:
+        "https://yt3.ggpht.com/HGes9hrn9xm1pv_2RiQsxHaGMvkH15wMVASJIXbIREhJYe_cF9UBnIwHPyAQ33UVN4qS6S2i=s900-c-k-c0x00ffffff-no-rj",
+      imageAlt: "",
+    },
+    {
+      id: "4",
+      to: "/share/lohoues",
+      title: "SORTEZZZ Emma Lohoues en colère répond à Carmen Sama en direct",
+      imageSrc:
+        "https://cdn.afrique-sur7.ci/700x400/articles/2022/05/emma-lohoues-son-pere-euge%CC%80ne-1er.jpg",
+      imageAlt: "",
+    },
+  ];
   return (
     <>
-          <article>
+      <article>
         <div className="bg-black">
           <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
             <p className="mt-2 text-xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
@@ -59,7 +61,7 @@ export default function ShareItems() {
                       className="w-full h-56 object-center object-cover group-hover:opacity-75"
                     />
                   </div>
-                  <br/>
+                  <br />
                   <p className="mt-1 text-lg font-medium text-white">
                     {item.title}
                   </p>
@@ -70,5 +72,5 @@ export default function ShareItems() {
         </div>
       </article>
     </>
-  )
+  );
 }
