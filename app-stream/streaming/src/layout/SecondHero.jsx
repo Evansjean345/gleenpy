1b2c9d358a35ca4ced791fdf57e9704d942d18b3
evansjean345 <evansjean345@gmail.com>
2025-02-11 11:50:58 +0000

    files

--- app-stream/streaming/src/layout/SecondHero.jsx
+++ app-stream/streaming/src/layout/SecondHero.jsx
@@ -4,10 +4,10 @@ export default function SecondHero() {
   return (
     <div className="bg-black">
       <article>
-        <div className="bg-black">
+        <div className="bg-[url('/nest_banner.png')] bg-cover">
           <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 className="text-xl font-extrabold tracking-tight text-white sm:text-3xl">
-              <span className="block ">
+              <span className="sm:block hidden">
                 Retrouvez vos films et series préférés sur NEXTDEO
               </span>
             </h2>
