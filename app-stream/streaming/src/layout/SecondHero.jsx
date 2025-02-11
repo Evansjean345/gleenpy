5c935795241ee5f45a2511a2a8a626b81b1fced8
evansjean345 <evansjean345@gmail.com>
2025-02-11 15:30:43 +0000

    slider

--- app-stream/streaming/src/layout/SecondHero.jsx
+++ app-stream/streaming/src/layout/SecondHero.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 export default function SecondHero() {
   return (
-    <div className="bg-black">
+    <div className="bg-black my-6 sm:my-24">
       <article>
-        <div className="bg-[url('/nest_banner.png')] bg-cover">
+        <div className="bg-[url('/nest_banner.png')]  sm:h-96 bg-contain bg-no-repeat sm:bg-cover">
           <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-            <h2 className="text-xl font-extrabold tracking-tight text-white sm:text-3xl">
+            {/*
+             <h2 className="text-xl font-extrabold tracking-tight text-white sm:text-3xl">
               <span className="sm:block hidden">
                 Retrouvez vos films et series préférés sur NEXTDEO
               </span>
-            </h2>
+            </h2> */}
             <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0"></div>
           </div>
         </div>
